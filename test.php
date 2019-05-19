285dc0236c7b6717d26b76e7c34fa8ddcf320b06
Zuhair <zuhair_m_h@yahoo.com>
2019-05-19 14:24:53 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,8 @@
 <?php
-if (isset($_POST['submit'])) {  
+
     echo "Your firstname is " . $_POST['fullname'] . "<br>";
 
     echo "You email is " . $_POST['email'] ;
 
-   }  else {
-
-    echo "Your form is not submitted yet please fill the form and visit again";
-  }
-
+  
 ?>
