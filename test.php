30586780ccc45ab452433eba2d609518de3c049c
Zuhair <zuhair_m_h@yahoo.com>
2019-05-19 14:13:41 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
 <?php
-echo("Welcome");
+if (isset($_POST['submit'])) {  
+    echo "Your firstname is " . $_POST['fullname'] . "<br>";
+
+    echo "You email is " . $_POST['email'] ;
+
+   }  else {
+
+    echo "Your form is not submitted yet please fill the form and visit again";
+  }
+
 ?>
