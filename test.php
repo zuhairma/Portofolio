6512951816b694d7ed0c8c1183e079a6e5fbc80c
Zuhair <zuhair_m_h@yahoo.com>
2019-05-19 14:03:49 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,3 @@
 <?php
- if(isset($_POST['submit']))
-{
-  $name = $_POST['fullname'];
-  $mailfrom = $_POST['email'];
-  $sub = $_POST['sub'];
-  $comments = $_POST['comments'];
-  $mailTo = "zuhair_m_h@yahoo.com";
-  $headers = "From: ".$mailfrom 
-    $txt = "You've received email from ".$name .\n\n".$comments;
-  mail($mailTo, $sub, $txt, $headers);
-  header("Location: index.html");
-}
-
+echo("Welcome");
 ?>
