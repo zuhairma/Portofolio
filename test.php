d6bcf16f462fa3c7d3e0d4f6028a34eebfbadff2
Zuhair <zuhair_m_h@yahoo.com>
2019-05-19 00:36:23 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,15 @@
 <?php
-$fullname = $_POST['fullname'];
-$email = $_POST['email'];
-$sub = $_POST['sub'];
-$comments = $_POST['comments'];
-
-$email_from = "Zuhairm@outlook.com";
-$subject    = "New Form Submission";
-$email_body = "Username: $fullname.\n".
-              "User Email: $email.\n".
-              "Subject: $sub.\n".
-              "User Comments: $comments.\n".
-$to = "Zuhair_m_h@yahoo.com";
-
-$headers = "From: $email_from \r\n";
-$headers .="Reply-to: $email \r\n";
-mail($to, $subject, $email_body, $headers);
-header("Location: index.html");
-
+if(isset($_POST['submit']))
+{
+  $name = $_POST['fullname'];
+  $mailfrom = $_POST['email'];
+  $sub = $_POST['sub'];
+  $comments = $_POST['comments'];
+  $mailTo = "zuhair_m_h@yahoo.com";
+  $headers = "From: " .$mailfrom 
+    $txt = "You've received email from ".$name .\n\n" .$comments;
+  mail($mailTo, $sub, $txt, $headers);
+  header("Location: index.php?mailsend");
+}
 
 ?>
