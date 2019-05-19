01108c4fe01c0fbddacf70da3062423236b21856
Zuhair <zuhair_m_h@yahoo.com>
2019-05-19 14:48:48 -0500

    Update test.php

--- test.php
+++ test.php
@@ -8,12 +8,12 @@
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
     
-    if ($_POST['submit']) {
+
     if (mail ($to, $subject, $body, $from)) { 
         echo '<p>Your message has been sent!</p>';
     } else { 
         echo '<p>Something went wrong, go back and try again!</p>'; 
     }
-}
+
   
 ?>
