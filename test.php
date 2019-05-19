72042f479531daf0b12513c16de6335cd002c074
Zuhair <zuhair_m_h@yahoo.com>
2019-05-19 16:38:19 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,5 @@
 <?php
-    $name = $_POST['fullname'];
-    $email = $_POST['email'];
-    $message = $_POST['comments'];
-    $from='zuhairm@outlook.com';
-    $to = 'zuhair_m_h@yahoo.com; 
-    $subject = 'Hello';
-
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-    
-
-mail ($to, $subject, $body, $from)
+  
         echo '<p>Your message has been sent!</p>';
    
   
